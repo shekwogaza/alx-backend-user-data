79a10a7c293fff1bcd36e1c9e20e5712c89fc1b2
GEEGII <gazasolomon@gmail.com>
2024-08-28 22:09:47 +0100

    Checking valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,8 +6,28 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    # Generate a salt
+    """Generate a salt
+
+    Args:
+        password (str): _description_
+
+    Returns:
+        bytes: _description_
+    """
     salt = bcrypt.gensalt()
     # Hash the password with the salt
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the provided password matches the hashed password
+
+    Args:
+        hashed_password (bytes): _description_
+        password (str): _description_
+
+    Returns:
+        bool: _description_
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
