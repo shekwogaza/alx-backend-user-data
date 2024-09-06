0f65371ee4fad3f95e45699046db4c5142774cf1
GEEGII <gazasolomon@gmail.com>
2024-09-06 21:19:48 +0100

    Hnadling Basic Auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -24,7 +24,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 auth_type = getenv("AUTH_TYPE")
-if auth_type:
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth_type:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API.
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class to manage API basic authentication."""
+    pass
