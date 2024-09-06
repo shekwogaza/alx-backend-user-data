4b64993a77b1d9624d06aeeef013cebfc20f7a3f
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-09-06 17:20:07 +0000

    added somemore documentations to app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """Route module for the API
 
+This module sets up a Flask application with CORS support and defines
+error handlers for 404 and 401 errors. It also registers the app_views
+blueprint and starts the Flask development server if run as the main script.
+
 Returns:
-    _type_: _description_
+    Flask: The configured Flask application instance.
 """
 
 from os import getenv
@@ -22,11 +26,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 def not_found(error) -> str:
     """Not found handler
 
+    This function handles 404 Not Found errors and returns a JSON response.
+
     Args:
-        error (_type_): _description_
+        error (werkzeug.exceptions.NotFound): The 404 error object.
 
     Returns:
-        str: _description_
+        tuple: A tuple containing a JSON response and the HTTP status code 404.
     """
     return jsonify({"error": "Not found"}), 404
 
@@ -35,16 +41,20 @@ def not_found(error) -> str:
 def unauthorized_error(error) -> str:
     """Handle 401 Unauthorized errors
 
+    This function handles 401 Unauthorized errors and returns a JSON response.
+
     Args:
-        error (_type_): _description_
+        error (werkzeug.exceptions.Unauthorized): The 401 error object.
 
     Returns:
-        tuple[str, Literal[401]]: _description_
+        tuple: A tuple containing a JSON response and the HTTP status code 401.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
+    # Get host and port from environment variables or use default values
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
+    # Run the Flask development server
     app.run(host=host, port=port)
