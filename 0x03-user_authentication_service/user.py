e788322f17b5b6c0e696e48aab8279cc882b1f1f
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-09-14 00:14:38 +0000

    Refactor user.py and db.py for SQLAlchemy implementation

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,81 +1,16 @@
 #!/usr/bin/env python3
-"""Flask app for user authentication and session management"""
-from auth import Auth
-from flask import Flask, abort, jsonify, request, redirect, url_for
+"""SQLAlchemy User model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
-AUTH = Auth()
+Base = declarative_base()
 
-@app.route("/")
-def home() -> str:
-    """Home endpoint"""
-    return jsonify({"message": "Bienvenue"})
+class User(Base):
+    """User table model"""
+    __tablename__ = 'users'
 
-@app.route("/sessions", methods=["POST"])
-def login():
-    """Login endpoint"""
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if not AUTH.valid_login(email, password):
-        abort(401)
-    session_id = AUTH.create_session(email)
-    response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie("session_id", session_id)
-    return response
-
-@app.route("/sessions", methods=["DELETE"])
-def logout():
-    """Logout endpoint"""
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if not user:
-        abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect(url_for("home"))
-
-@app.route("/users", methods=["POST"])
-def users():
-    """New user signup endpoint"""
-    email = request.form.get("email")
-    password = request.form.get("password")
-    try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
-
-@app.route("/profile")
-def profile() -> str:
-    """User profile endpoint"""
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if not user:
-        abort(403)
-    return jsonify({"email": user.email})
-
-@app.route("/reset_password", methods=["POST"])
-def get_reset_password_token() -> str:
-    """Reset password token endpoint"""
-    email = request.form.get("email")
-    try:
-        reset_token = AUTH.get_reset_password_token(email)
-    except ValueError:
-        abort(403)
-    return jsonify({"email": email, "reset_token": reset_token})
-
-@app.route("/reset_password", methods=["PUT"])
-def update_password():
-    """Password update endpoint"""
-    email = request.form.get("email")
-    new_password = request.form.get("new_password")
-    reset_token = request.form.get("reset_token")
-    try:
-        AUTH.update_password(reset_token, new_password)
-    except ValueError:
-        abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
